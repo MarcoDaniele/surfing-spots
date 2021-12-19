5eaf040e4ab46c9d0749cc75e7fc7d83519b5023
Marco Daniele <marcodaniele@live.it>
2021-12-19 13:32:37 +0100

    WIP: build ui prototype along with dummy data

--- Surfing Spots/ContentView.swift
+++ Surfing Spots/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    let cities: [City: Weather]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List([City](cities.keys)) { city in
+                
+                ZStack {
+                    Color.black
+                        .opacity(0.2)
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Spacer(minLength: UIScreen.main.bounds.height/8)
+                            Text(city.name)
+                                .font(.title)
+                            Text("\(cities[city]!.description) - \(cities[city]!.temperature) degrees")
+                                .font(.footnote)
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                }
+                .background(
+                    Image(uiImage: cities[city]!.isSunny ? city.image : UIImage())
+                        .resizable()
+                )
+                .foregroundColor(.white)
+                .cornerRadius(16)
+                .listRowSeparator(.hidden)
+                .listRowInsets(.none)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Surfing Spots")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static let mockCities: [City: Weather] = [City(name: "Riccione", image: UIImage(named: "city1")!): Weather(temperature: 30), City(name: "Miami", image: UIImage(named: "city2")!): Weather(temperature: 30), City(name: "Los Angeles", image: UIImage(named: "city3")!): Weather(temperature: 30)]
+    
     static var previews: some View {
-        ContentView()
+        ContentView(cities: mockCities)
+    }
+}
+
+
+struct City: Identifiable, Hashable, Equatable {
+    let id = UUID()
+    let name: String
+    let image: UIImage
+}
+
+struct Weather: Equatable {
+    var temperature: Int
+    var isSunny: Bool {
+        temperature >= 30
+    }
+    var description: String {
+        isSunny ? "sunny" : "cloudy"
     }
 }
--- Surfing Spots/Surfing_SpotsApp.swift
+++ Surfing Spots/Surfing_SpotsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Surfing_SpotsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(cities: [:])
         }
     }
 }
