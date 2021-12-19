37bd437d33b519a63dd877841834a2f55e32cd72
Marco Daniele <marcodaniele@live.it>
2021-12-19 12:36:07 +0100

    Initial Commit

---
+++ Surfing Spots/Surfing_SpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Surfing_SpotsApp.swift
+//  Surfing Spots
+//
+//  Created by Marco Daniele on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Surfing_SpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
